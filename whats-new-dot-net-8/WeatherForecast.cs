f643281abb89ba0be80d7ee6875152e4cc4ff9d3
Hemant <hemant.shelar@gmail.com>
2024-11-20 16:09:35 +1100

    Initial code.

---
+++ whats-new-dot-net-8/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace whats_new_dot_net_8
+{
+	public class WeatherForecast
+	{
+		public DateOnly Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+		public string? Summary { get; set; }
+	}
+}
